d55a79336c76b08f839d61933f05c50c0b550229
James Higgins and Ken Rutan <james@>
2017-06-15 13:33:58 -0700

    add ability to log in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth]
 })
+
 export class AppComponent {
+
+  user: Observable<firebase.User>;
   title = 'Greenlight Crowdfunding!';
+
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  login(email, password) {
+    // this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  ngOnInit() {
+    console.log(this.user);
+  }
+
 }
